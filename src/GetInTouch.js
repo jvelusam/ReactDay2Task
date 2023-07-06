c551f3abe4540ee5bccd113f579d5ada58a9f595
Jayaveerasabari Velusamy <jayaveerasabarivelusamy@gmail.com>
2023-07-06 09:39:00 +0530

    first commit

---
+++ src/GetInTouch.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import GetInTouchCard from "./GetInTouchCard";
+import data from "./data.json";
+export default function GetInTouch()
+{
+    return( <section className="contact bg-success ">
+    <div className="container ">
+      <h2 className="text-white">We love new friends!</h2>
+      <div className="row">
+        {data.map((value, index) => {
+          return (
+            <GetInTouchCard
+              key={index}
+              imageUrl={value.imageUrl}
+              city={value.city}
+            />
+          );
+        })}
+      </div>
+    </div>
+  </section>);
+
+}
---
+++ src/GetInTouchCard.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+
+export default function GetInTouchCard({ imageUrl, city })
+{
+
+return( <div className="col-4">
+<div className="card" style={{ width: "18rem" }}>
+  <img src={imageUrl} className="card-img-top" alt="..." />
+  <div className="card-body">
+    <h5 className="card-title">{city}</h5>
+    <p className="card-text">
+      {city === "London"
+        ? "London is a beautiful city"
+        : "sndkjanskdnask"}
+    </p>
+    <a
+      href="#"
+      className={
+        city === "London"
+          ? "btn btn-danger"
+          : city === "Paris"
+          ? "btn btn-warning"
+          : "btn btn-success"
+      }
+    >
+      Go somewhere
+    </a>
+  </div>
+</div>
+</div>);
+
+}
